9017270d65e0651cf0addc482d882e3c733e8f47
jayang <wotjd5101@gmail.com>
2024-05-08 03:30:50 -0400

    Update baseDir and Create input function

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,3 +1,17 @@
+
+let generateTable = () => {
+    const size = parseInt(window.prompt(`Enter table size: `, `10`));
+    let value = 0;
+
+    if(isNaN(size) || size < 1) {
+        alert(`Invalid input. Please try again...`);
+        return;
+    }
+
+    console.log(size);
+
+};
+
 window.onload = () => {
-    // Add your code here
+    generateTable();
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,11 @@
-const { src, dest, series, watch } = require(`gulp`),
-    CSSLinter = require(`gulp-stylelint`),
-    del = require(`del`),
-    babel = require(`gulp-babel`),
-    htmlCompressor = require(`gulp-htmlmin`),
-    htmlValidator = require(`gulp-html`),
-    jsCompressor = require(`gulp-uglify`),
-    jsLinter = require(`gulp-eslint`),
-    sass = require(`gulp-sass`)(require(`sass`)),
+const {series, watch, src, dest} = require(`gulp`),
     browserSync = require(`browser-sync`),
-    reload = browserSync.reload;
+    jsLinter = require(`gulp-eslint`),
+    babel = require(`gulp-babel`),
+    jsCompressor = require(`gulp-uglify`),
+    CSSLinter = require(`gulp-stylelint`),
+    concat = require(`gulp-concat`),
+    cleanCss = require(`gulp-clean-css`);
 
 let browserChoice = `default`;
 
@@ -21,8 +18,7 @@ async function chrome () {
 }
 
 async function edge () {
-    // In Windows, the value might need to be “microsoft-edge”. Note the dash.
-    browserChoice = `microsoft edge`;
+    browserChoice = `microsoft-edge`;
 }
 
 async function firefox () {
@@ -45,7 +41,7 @@ async function allBrowsers () {
     browserChoice = [
         `brave browser`,
         `google chrome`,
-        `microsoft edge`, // Note: In Windows, this might need to be microsoft-edge
+        `microsoft-edge`, // Note: In Windows, this might need to be microsoft-edge
         `firefox`,
         `opera`,
         `safari`,
@@ -53,65 +49,30 @@ async function allBrowsers () {
     ];
 }
 
-let validateHTML = () => {
-    return src([`dev/html/*.html`, `dev/html/**/*.html`])
-        .pipe(htmlValidator(undefined));
-};
-
-let compileCSSForDev = () => {
-    return src(`dev/styles/scss/main.scss`)
-        .pipe(sass.sync({
-            outputStyle: `expanded`,
-            precision: 10
-        }).on(`error`, sass.logError))
-        .pipe(dest(`temp/styles`));
-};
-
 let lintJS = () => {
-    return src(`dev/scripts/*.js`)
-        .pipe(jsLinter())
+    return src(`dev/js/*.js`)
+        .pipe(
+            jsLinter({ configFile: `.eslintrc`})
+        )
         .pipe(jsLinter.formatEach(`compact`));
 };
+
+let lintCSS = () => {
+    return src(`dev/css/*.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ],
+            configFile: `.stylelintrc`
+        })
+        );
+};
+
 let transpileJSForDev = () => {
-    return src(`dev/scripts/*.js`)
+    return src(`dev/js/*.js`)
         .pipe(babel())
-        .pipe(dest(`temp/scripts`));
-};
-
-let compressHTML = () => {
-    return src([`dev/html/*.html`, `dev/html/**/*.html`])
-        .pipe(htmlCompressor({collapseWhitespace: true}))
-        .pipe(dest(`prod`));
-};
-
-let compileCSSForProd = () => {
-    return src(`dev/styles/scss/main.scss`)
-        .pipe(sass.sync({
-            outputStyle: `compressed`,
-            precision: 10
-        }).on(`error`, sass.logError))
-        .pipe(dest(`prod/styles`));
-};
-
-let transpileJSForProd = () => {
-    return src(`dev/scripts/*.js`)
-        .pipe(babel())
-        .pipe(jsCompressor())
         .pipe(dest(`prod/scripts`));
-};
-
-let copyUnprocessedAssetsForProd = () => {
-    return src([
-        `dev/*.*`,       // Source all files,
-        `dev/**`,        // and all folders,
-        `!dev/html/`,    // but not the HTML folder
-        `!dev/html/*.*`, // or any files in it
-        `!dev/html/**`,  // or any sub folders;
-        `!dev/img/`,     // ignore images;
-        `!dev/**/*.js`,  // ignore JS;
-        `!dev/styles/**` // and, ignore Sass/CSS.
-    ], {dot: true})
-        .pipe(dest(`prod`));
 };
 
 let serve = () => {
@@ -121,76 +82,34 @@ let serve = () => {
         browser: browserChoice,
         server: {
             baseDir: [
-                `temp`,
                 `dev`,
-                `dev/html`
+                `dev/html`,
+                `dev/styles`,
+                `dev/css`
             ]
         }
     });
 
-    watch(`dev/scripts/*.js`, series(lintJS, transpileJSForDev))
-        .on(`change`, reload);
-
-    watch(`dev/styles/scss/**/*.scss`, compileCSSForDev)
-        .on(`change`, reload);
-
-    watch(`dev/html/**/*.html`, validateHTML)
-        .on(`change`, reload);
-
-    watch(`dev/img/**/*`)
-        .on(`change`, reload);
+    watch(`dev/js/*.js`).on(`change`, browserSync.reload);
+    watch(`dev/css/*.css`).on(`change`, browserSync.reload);
+    watch(`dev/html/*.html`).on(`change`, browserSync.reload);
 };
 
-async function clean() {
-    let fs = require(`fs`),
-        i,
-        foldersToDelete = [`./temp`, `prod`];
-
-    for (i = 0; i < foldersToDelete.length; i++) {
-        try {
-            fs.accessSync(foldersToDelete[i], fs.F_OK);
-            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
-                ` directory was found and will be deleted.\n`);
-            del(foldersToDelete[i]);
-        } catch (e) {
-            process.stdout.write(`\n\tThe ` + foldersToDelete[i] +
-                ` directory does NOT exist or is NOT accessible.\n`);
-        }
-    }
-
-    process.stdout.write(`\n`);
-}
-
-async function listTasks () {
-    let exec = require(`child_process`).exec;
-
-    exec(`gulp --tasks`, function (error, stdout, stderr) {
-        if (null !== error) {
-            process.stdout.write(`An error was likely generated when invoking ` +
-                `the “exec” program in the default task.`);
-        }
-
-        if (`` !== stderr) {
-            process.stdout.write(`Content has been written to the stderr stream ` +
-                `when invoking the “exec” program in the default task.`);
-        }
-
-        process.stdout.write(`\n\tThis default task does ` +
-            `nothing but generate this message. The ` +
-            `available tasks are:\n\n${stdout}`);
-    });
-}
-
-let lintCSS = () => {
-    return src(`dev/styles/css/**/*.css`)
-        .pipe(CSSLinter({
-            failAfterError: false,
-            reporters: [
-                {formatter: `string`, console: true}
-            ]
-        }));
+let compileCSSForProd = () => {
+    return src(`dev/css/*.css`)
+        .pipe(concat(`app.css`))
+        .pipe(cleanCss())
+        .pipe(dest(`prod/styles`));
 };
 
+let transpileJSForProd = () => {
+    return src(`dev/js/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
+
+exports.default = serve;
 exports.brave = series(brave, serve);
 exports.chrome = series(chrome, serve);
 exports.edge = series(edge, serve);
@@ -199,27 +118,19 @@ exports.opera = series(opera, serve);
 exports.safari = series(safari, serve);
 exports.vivaldi = series(vivaldi, serve);
 exports.allBrowsers = series(allBrowsers, serve);
-exports.validateHTML = validateHTML;
-exports.compileCSSForDev = compileCSSForDev;
+exports.default = serve;
 exports.lintJS = lintJS;
+exports.lintCSS = lintCSS;
 exports.transpileJSForDev = transpileJSForDev;
-exports.compressHTML = compressHTML;
 exports.compileCSSForProd = compileCSSForProd;
 exports.transpileJSForProd = transpileJSForProd;
-exports.copyUnprocessedAssetsForProd = copyUnprocessedAssetsForProd;
-exports.clean = clean;
-exports.default = listTasks;
-exports.lintCSS = lintCSS;
 exports.serve = series(
-    validateHTML,
-    compileCSSForDev,
     lintJS,
-    transpileJSForDev,
-    serve
+    serve,
+    lintCSS,
+    transpileJSForDev
 );
-exports.build = series(
-    compressHTML,
+exports.build   = series(
     compileCSSForProd,
-    transpileJSForProd,
-    copyUnprocessedAssetsForProd
+    transpileJSForProd
 );
