92b1b09ea2910d182b9ab38ac74aa49bb9b8ef75
jayang <wotjd5101@gmail.com>
2024-05-08 06:38:18 -0400

    Update diagonal line

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,12 +1,19 @@
 let generateTable = () => {
+    const projectTitleHeader = document.createElement(`h2`);
+    projectTitleHeader.appendChild(document.createTextNode(`Flipping a Matrix Along a Diagonal`));
+    document.body.append(projectTitleHeader);
 
-    const originalTableHeader = document.createElement(`h2`);
+    const originalTableHeader = document.createElement(`h3`);
     originalTableHeader.appendChild(document.createTextNode(`Originl Matrix`));
     document.body.append(originalTableHeader);
 
     const createTableContainer = document.createElement(`div`);
     createTableContainer.id = `tableContainer`;
     document.body.appendChild(createTableContainer);
+
+    const flippedTitle = document.createElement(`h3`);
+    flippedTitle.textContent = `Flipped Matrix`;
+    document.body.appendChild(flippedTitle);
 
     const createFlippedTableContainer = document.createElement(`div`);
     createFlippedTableContainer.id = `flippedTableContainer`;
@@ -37,6 +44,10 @@ let generateTable = () => {
         for (let j = 0; j < size; j++) {
             const cell = document.createElement(`td`);
             cell.appendChild(document.createTextNode(`${value++}`));
+
+            if (j == size - 1 - i) {
+                cell.classList.add(`diagonal-line`);
+            }
             row.appendChild(cell);
 
             const tableContainer = document.createElement(`div`);
@@ -46,7 +57,6 @@ let generateTable = () => {
         }
         tableBody.appendChild(row);
         originalMatrix.push(rowValues);
-
     }
 
     table.appendChild(tableBody);
@@ -56,15 +66,16 @@ let generateTable = () => {
     const flippedTable = document.createElement(`table`);
     const flippedTableBody = document.createElement(`tbody`);
 
-    const flippedTableHeader = document.createElement(`h2`);
-    flippedTableHeader.appendChild(document.createTextNode(`Flipped Matrix`));
-    document.body.append(flippedTableHeader);
-
     for (let i = 0; i < flippedMatrix.length; i++) {
         const row = document.createElement(`tr`);
         for (let j = 0; j < flippedMatrix[i].length; j++) {
             const cell = document.createElement(`td`);
             cell.appendChild(document.createTextNode(`${flippedMatrix[i][j]}`));
+
+            if (j === size - 1 - i) {
+                cell.classList.add(`diagonal-line`);
+            }
+
             row.appendChild(cell);
         }
         flippedTableBody.appendChild(row);
