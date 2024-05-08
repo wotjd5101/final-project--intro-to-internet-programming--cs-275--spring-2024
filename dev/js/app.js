b7150312c7156381378140136da05045fdd487f6
jayang <wotjd5101@gmail.com>
2024-05-08 04:42:36 -0400

    create a table and update css

--- dev/js/app.js
+++ dev/js/app.js
@@ -1,5 +1,13 @@
-
 let generateTable = () => {
+
+    const originalTableHeader = document.createElement(`h2`);
+    originalTableHeader.appendChild(document.createTextNode(`Originl Matrix`));
+    document.body.append(originalTableHeader);
+
+    const createTableContainer = document.createElement(`div`);
+    createTableContainer.id = `tableContainer`;
+    document.body.appendChild(createTableContainer);
+
     const size = parseInt(window.prompt(`Enter table size: `, `10`));
     let value = 0;
 
@@ -8,8 +16,26 @@ let generateTable = () => {
         return;
     }
 
-    console.log(size);
+    const tableContainer = document.getElementById(`tableContainer`);
+    tableContainer.innerHTML = ``;
 
+    const table = document.createElement(`table`);
+    const tableBody = document.createElement(`tbody`);
+
+    for (let i = 0; i < size; i++) {
+        const row = document.createElement(`tr`);
+        for (let j = 0; j < size; j++) {
+            const cell = document.createElement(`td`);
+            cell.appendChild(document.createTextNode(`${value++}`));
+            const tableContainer = document.createElement(`div`);
+            tableContainer.id = `tableContainer`;
+            row.appendChild(cell);
+        }
+        tableBody.appendChild(row);
+    }
+
+    table.appendChild(tableBody);
+    tableContainer.appendChild(table);
 };
 
 window.onload = () => {
